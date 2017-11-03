c6c85177214e254b742596ef881ac811ddef37d2
rohitkumar1989 <rohit.asr1989@gmail.com>
2017-11-03 15:21:05 +0530

    Initial commit

---
+++ program1.cpp
@@ -0,0 +1,89 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+//#include<abc.h>
+using namespace std;
+/*class visitor1;
+class visitor2;
+class visitor;*/
+
+class color{
+    public:
+    virtual void accept(class visitor* v)=0;
+    
+};
+
+class red: public color{
+    public: 
+            void accept(class visitor* v);
+};
+
+class blue : public color{
+    public:
+            void accept(class visitor* v);
+};
+
+class visitor{
+    public:
+            virtual void visit(blue*)=0;
+            virtual void visit(red*)=0;
+};
+
+class visitor1: public visitor{
+    public:
+            void visit(blue* b);
+            void visit(red* r);
+};
+
+class visitor2: public visitor{
+    public:
+            void visit(blue* b);
+            void visit(red* r);
+};
+
+
+void red::accept(class visitor* v){
+    v->visit(this);    
+}
+    
+void blue::accept(class visitor* v){
+    v->visit(this);
+}
+          
+
+void visitor1::visit(blue* b){
+    cout<<"visitor 1 blue"<<endl;
+}
+            
+void visitor1::visit(red* r){
+    cout<<"visitor 1 red"<<endl;
+}
+ 
+ void visitor2::visit(blue* b){
+    cout<<"visitor 1 blue"<<endl;
+}
+            
+void visitor2::visit(red* r){
+    cout<<"visitor 1 red"<<endl;
+}
+     
+
+int main()
+{
+   color *c[]={new blue, new red};
+   visitor1 v1;
+   visitor2 v2;
+   c[0]->accept(&v1);
+   c[1]->accept(&v2);
+
+    return 0;
+}
+
+
+
