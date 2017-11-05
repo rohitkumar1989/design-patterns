4df830ff55ac8ac5b55440313e17737a7c42aa15
rohitkumar1989 <rohit.asr1989@gmail.com>
2017-11-05 20:24:05 +0530

    design pattern visitor program2

---
+++ program2.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+//#include<program2.h>
+using namespace std;
+
+class element{
+	const char* strelement;
+    public: 
+		  element(const char* str):strelement(str){}
+	      virtual void accept(class visitor&)=0;	
+           void print(const char* str){
+			 cout<<str<<" "<<strelement<<endl;
+		 }
+};
+
+class that: public element{
+	public: 
+		   that(const char* strtemp):element(strtemp){}
+	       void accept(class visitor&);
+};
+
+class those: public element{
+	public: 
+	       those(const char* strtemp):element(strtemp){}
+		   void accept(class visitor&);
+};
+
+class visitor{
+	public:
+		   virtual void visit(those*)=0;
+		   virtual void visit(that*)=0;
+};
+
+class visit1:public visitor{
+	public:
+		   void visit(those*);
+		   void visit(that*);
+};		
+
+class visit2:public visitor{
+	public:
+		   void visit(those*);
+		   void visit(that*);
+};
+
+void those::accept(visitor& v){
+	v.visit(this);
+}
+
+void that::accept(visitor& v){
+	v.visit(this);
+}
+
+void visit1::visit(those* c){
+		c->print("visit one");
+}
+
+void visit1::visit(that* c){
+		c->print("visit two");
+}
+
+void visit2::visit(those* c){
+		c->print("visit one");
+}
+
+void visit2::visit(that* c){
+		c->print("visit two");
+}
+
+
+int main()
+{
+	visit1 v1;
+	visit2 v2;
+	volatile int size=4;
+	element *e[size]={new that("that"),new those("those"),new that("that"),new those("those")};
+	//element *e[size]={new that("that"),new those("those"),&that("that"),new those("those")};
+	//cannot take address of temporary
+	for(int i=0;i<size;i++)
+	{
+		e[i].accept(v1);	
+		e[i]->accept(v2);	
+				
+	}
+	
+	
+	return 0;
+}
